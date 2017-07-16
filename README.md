60867b8c9ac52bb3d4be7edba2ab5c9dd967c8da
Viktor Moreno <victor@>
2017-07-16 05:07:17 +0100

    added README, started to build error handling

--- README.md
+++ README.md
@@ -19,16 +19,8 @@ deployment status of projects in your jenkins machine.
 The project is articulated to be a public/private single page, displaying
 your jenkins' works last build status with a semaphore strategy.
 
-Oh by the way, the curl calls are not error proof, if you don't get a jenkins object, it will break the world, next
-version will fix that :)
 
 Thanks
 ======================
 Thanks to [http://www.brokenpixel.uk (John James)](http://www.brokenpixel.uk) to let me use the CurlBundle.
- 
-TODOs
-======================
-
-* Control on bad responses
-* Individual status query.
 
--- src/MerkCorp/JenkinsMonitorBundle/Controller/DefaultController.php
+++ src/MerkCorp/JenkinsMonitorBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
@@ -17,22 +18,47 @@ class DefaultController extends Controller
      * @Route("/", name="wk_index")
      * @Template()
      */
-    public function indexAction() : array
+    public function indexAction()
     {
 
         $projectTypes = $this->getParameter('api_type_projects');
         $jenkinsJobs = array();
 
-        if(!empty($projectTypes)) {
-            foreach($projectTypes as $projectKey => $projectType) {
+        if (!empty($projectTypes)) {
+            foreach ($projectTypes as $projectKey => $projectType) {
                 $jenkinsJobs[$projectKey] = $this->getProjects($projectType);
+                if (array_key_exists('error', $jenkinsJobs[$projectKey]) && $jenkinsJobs[$projectKey]['error'] === true) {
+                    return $this->redirectToRoute('wk_curl_error_page', array(
+                        'errorCode' => $jenkinsJobs[$projectKey]['http_code'],
+                    ));
+                }
             }
-        }else{
+        } else {
             $jenkinsJobs['all'] = $this->getProjects();
+            if (array_key_exists('error', $jenkinsJobs['all']) && $jenkinsJobs['all']['error'] === true) {
+                return $this->redirectToRoute('wk_curl_error_page', array(
+                    'errorCode' => $jenkinsJobs['all']['http_code'],
+                ));
+            }
         }
 
         return array(
             'jenkins_jobs' => $jenkinsJobs
+        );
+    }
+
+    /**
+     * Error page to handle curl bad responses.
+     * @param $errorCode
+     * @Route("/curl/error/{errorCode}", name="wk_curl_error_page")
+     * @Template()
+     * @return array
+     */
+
+    public function errorPageAction($errorCode)
+    {
+        return array(
+            'error' => $errorCode
         );
     }
 
@@ -47,9 +73,9 @@ class DefaultController extends Controller
     public function projectExtendedInformationAction(string $projectName, bool $statusOnly = false)
     {
         $curler = $this->get('curler');
-        $urlToCurl = $this->generateBaseUrl().'job/'.$projectName.'/lastBuild/api/json';
+        $urlToCurl = $this->generateBaseUrl() . 'job/' . $projectName . '/lastBuild/api/json';
 
-        if($statusOnly) {
+        if ($statusOnly) {
             $urlToCurl .= '?tree=result';
         }
 
@@ -65,9 +91,10 @@ class DefaultController extends Controller
      * @param string $projectName
      * @return JsonResponse
      */
-    public function remoteBuildJobAction(string $projectName){
+    public function remoteBuildJobAction(string $projectName)
+    {
         $curler = $this->get('curler');
-        $urlToCurl = $this->generateBaseUrl().'job/'.$projectName.'/build';
+        $urlToCurl = $this->generateBaseUrl() . 'job/' . $projectName . '/build';
 
         $curler->curlAUrl($urlToCurl);
         return new JsonResponse("Build triggered...");
@@ -78,12 +105,12 @@ class DefaultController extends Controller
      * the information supplied in the parameters.yml
      * @return string
      */
-    private function generateBaseUrl() : string
+    private function generateBaseUrl(): string
     {
         return $this->getParameter('api_schema')
             . '://'
             . $this->getParameter('api_user')
-            .':' . $this->getParameter('api_token')
+            . ':' . $this->getParameter('api_token')
             . '@'
             . $this->getParameter('api_domain');
     }
@@ -96,19 +123,27 @@ class DefaultController extends Controller
      *
      * @return array
      */
-    private function getProjects(string $projectType = null) : array
+    private function getProjects(string $projectType = null): array
     {
         $curler = $this->get('curler');
         $baseUrl = $this->generateBaseUrl();
 
-        $urlToCurl = $baseUrl.'api/json';
-        if($projectType) {
-            $urlToCurl = $baseUrl.'view/'.rawurlencode($projectType).'/api/json';
+        $urlToCurl = $baseUrl . 'api/json';
+        if ($projectType) {
+            $urlToCurl = $baseUrl . 'view/' . rawurlencode($projectType) . '/api/json';
         }
 
         $liveProjectsResult = $curler->curlAUrl($urlToCurl);
 
+        $curlCheck = $this->manageCurlHttpResponseCodes($liveProjectsResult['curlInfo']);
+
+        if ($curlCheck['error'] === true) {
+            return $curlCheck;
+        }
+
         $liveProjectsArr = json_decode($liveProjectsResult['body'], true);
+
+
         return $liveProjectsArr['jobs'];
     }
 
@@ -119,14 +154,14 @@ class DefaultController extends Controller
      *
      * @return array
      */
-    private function getLastBuildInformation(array $liveProjects) : array
+    private function getLastBuildInformation(array $liveProjects): array
     {
         // We iterate over the array passing the item by reference to being
         // able to manipulate it.
-        array_walk($liveProjects, function(&$item) {
+        array_walk($liveProjects, function (&$item) {
 
             $curler = $this->get('curler');
-            $urlToCurl = $this->generateBaseUrl().'job/'.$item['name'].'/lastBuild/api/json';
+            $urlToCurl = $this->generateBaseUrl() . 'job/' . $item['name'] . '/lastBuild/api/json';
 
             $lastBuildInfoResult = $curler->curlAUrl($urlToCurl);
             // We add a new key to store all the build information
@@ -134,6 +169,33 @@ class DefaultController extends Controller
         });
 
         return $liveProjects;
+    }
+
+    private function manageCurlHttpResponseCodes($curlInfo)
+    {
+        $result = array(
+            'error' => null,
+            'message' => null,
+            'http_code' => 0,
+        );
+
+        switch ($curlInfo['http_code']) {
+            case 403:
+                $result['http_code'] = 403;
+                $result['error'] = true;
+                return $result;
+                break;
+            case 200:
+                $result['http_code'] = 200;
+                $result['error'] = false;
+                break;
+            default:
+                $result['http_code'] = $curlInfo['http_code'];
+                $result['error'] = true;
+
+        }
+
+        return $result;
     }
 
 }
